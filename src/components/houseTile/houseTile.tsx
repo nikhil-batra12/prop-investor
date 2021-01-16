fe0af8eae00e828fabde09958e6b5f77e463b9f9
nikhil-batra12 <nikhil.batra1210@gmail.com>
2021-01-17 01:05:11 +0530

    Home page setup

--- src/components/houseTile/houseTile.tsx
+++ src/components/houseTile/houseTile.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Button from "react-bootstrap/Button";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Image } from "react-bootstrap";
+import Carousel from "react-bootstrap/Carousel";
 
 class HouseTile extends React.PureComponent {
   render() {
@@ -9,54 +10,124 @@ class HouseTile extends React.PureComponent {
       <Container fluid>
         <h2>Properties for Sale</h2>
         <Row>
-          <Col xs={12} md={6} lg={3}>
-            <Jumbotron>
-              <h1>Hello, world!</h1>
-              <p>
-                This is a simple hero unit, a simple jumbotron-style component
-                for calling extra attention to featured content or information.
-              </p>
-              <p>
-                <Button variant="primary">Learn more</Button>
-              </p>
-            </Jumbotron>
-          </Col>
-          <Col xs={12} md={6} lg={3}>
-            <Jumbotron>
-              <h1>Hello, world!</h1>
-              <p>
-                This is a simple hero unit, a simple jumbotron-style component
-                for calling extra attention to featured content or information.
-              </p>
-              <p>
-                <Button variant="primary">Learn more</Button>
-              </p>
-            </Jumbotron>
-          </Col>
-          <Col xs={12} md={6} lg={3}>
-            <Jumbotron>
-              <h1>Hello, world!</h1>
-              <p>
-                This is a simple hero unit, a simple jumbotron-style component
-                for calling extra attention to featured content or information.
-              </p>
-              <p>
-                <Button variant="primary">Learn more</Button>
-              </p>
-            </Jumbotron>
-          </Col>
-          <Col xs={12} md={6} lg={3}>
-            <Jumbotron>
-              <h1>Hello, world!</h1>
-              <p>
-                This is a simple hero unit, a simple jumbotron-style component
-                for calling extra attention to featured content or information.
-              </p>
-              <p>
-                <Button variant="primary">Learn more</Button>
-              </p>
-            </Jumbotron>
-          </Col>
+          <Carousel className=" mr-auto ml-auto">
+            <Carousel.Item>
+              <Col xs={12} md={6} lg={3} className="d-inline-block">
+                <Jumbotron>
+                  <h1>Hello, world1</h1>
+                  <Image src="images/house1.jpeg" fluid />
+                  <p>
+                    This is a simple hero unit, a simple jumbotron-style
+                    component for calling extra attention to featured content or
+                    information.
+                  </p>
+                  <p>
+                    <Button variant="primary">Learn more</Button>
+                  </p>
+                </Jumbotron>
+              </Col>
+              <Col xs={12} md={6} lg={3} className="d-inline-block">
+                <Jumbotron>
+                  <h1>Hello, world2</h1>
+                  <Image src="images/house1.jpeg" fluid />
+                  <p>
+                    This is a simple hero unit, a simple jumbotron-style
+                    component for calling extra attention to featured content or
+                    information.
+                  </p>
+                  <p>
+                    <Button variant="primary">Learn more</Button>
+                  </p>
+                </Jumbotron>
+              </Col>
+              <Col xs={12} md={6} lg={3} className="d-inline-block">
+                <Jumbotron>
+                  <h1>Hello, world3</h1>
+                  <Image src="images/house1.jpeg" fluid />
+                  <p>
+                    This is a simple hero unit, a simple jumbotron-style
+                    component for calling extra attention to featured content or
+                    information.
+                  </p>
+                  <p>
+                    <Button variant="primary">Learn more</Button>
+                  </p>
+                </Jumbotron>
+              </Col>
+              <Col xs={12} md={6} lg={3} className="d-inline-block">
+                <Jumbotron>
+                  <h1>Hello, world4</h1>
+                  <Image src="images/house1.jpeg" fluid />
+                  <p>
+                    This is a simple hero unit, a simple jumbotron-style
+                    component for calling extra attention to featured content or
+                    information.
+                  </p>
+                  <p>
+                    <Button variant="primary">Learn more</Button>
+                  </p>
+                </Jumbotron>
+              </Col>
+            </Carousel.Item>
+            <Carousel.Item>
+              <Col xs={12} md={6} lg={3} className="d-inline-block">
+                <Jumbotron>
+                  <h1>Hello, world5</h1>
+                  <Image src="images/house2.jpg" fluid />
+                  <p>
+                    This is a simple hero unit, a simple jumbotron-style
+                    component for calling extra attention to featured content or
+                    information.
+                  </p>
+                  <p>
+                    <Button variant="primary">Learn more</Button>
+                  </p>
+                </Jumbotron>
+              </Col>
+              <Col xs={12} md={6} lg={3} className="d-inline-block">
+                <Jumbotron>
+                  <h1>Hello, world6</h1>
+                  <Image src="images/house2.jpg" fluid />
+                  <p>
+                    This is a simple hero unit, a simple jumbotron-style
+                    component for calling extra attention to featured content or
+                    information.
+                  </p>
+                  <p>
+                    <Button variant="primary">Learn more</Button>
+                  </p>
+                </Jumbotron>
+              </Col>
+              <Col xs={12} md={6} lg={3} className="d-inline-block">
+                <Jumbotron>
+                  <h1>Hello, world7</h1>
+                  <Image src="images/house2.jpg" fluid />
+                  <p>
+                    This is a simple hero unit, a simple jumbotron-style
+                    component for calling extra attention to featured content or
+                    information.
+                  </p>
+                  <p>
+                    <Button variant="primary">Learn more</Button>
+                  </p>
+                </Jumbotron>
+              </Col>
+              <Col xs={12} md={6} lg={3} className="d-inline-block">
+                <Jumbotron>
+                  <h1>Hello, world8</h1>
+                  <Image src="images/house2.jpg" fluid />
+                  <p>
+                    This is a simple hero unit, a simple jumbotron-style
+                    component for calling extra attention to featured content or
+                    information.
+                  </p>
+                  <p>
+                    <Button variant="primary">Learn more</Button>
+                  </p>
+                </Jumbotron>
+              </Col>
+            </Carousel.Item>
+          </Carousel>
         </Row>
       </Container>
     );
--- src/root/App.tsx
+++ src/root/App.tsx
@@ -2,13 +2,23 @@ import React from "react";
 import "./App.css";
 import Home from "modules/home";
 import NavigationPanel from "components/navigationPanel/navigationPanel";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <NavigationPanel />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <NavigationPanel />
+        <Container fluid>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
